c0ab84cf078bd48f8ddecd49ab4c43f1c311b000
Wilf Silver <git@wilfsilver.co.uk>
2025-04-13 22:46:04 +0100

    Remove the Suggest Changes button

--- src/config.ts
+++ src/config.ts
@@ -12,9 +12,9 @@ export const SITE = {
   showArchives: false,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Suggest Changes",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/WilfSilver/website/edit/main/",
   },
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
